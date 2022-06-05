2654f0a40c30e7a234d9a22dd048473fc584ab71
chosh2000 <cho303@purdue.edu>
2022-06-05 17:32:45 -0400

    j

--- IIC/code/utils/cluster/eval_metrics.py
+++ IIC/code/utils/cluster/eval_metrics.py
@@ -46,7 +46,7 @@ def _hungarian_match(flat_preds, flat_targets, preds_k, targets_k):
 
   # num_correct is small
   match = linear_assignment(num_samples - num_correct)
-  print(match)
+  print(match[0])
 
   # return as list of tuples, out_c to gt_c
   res = []
